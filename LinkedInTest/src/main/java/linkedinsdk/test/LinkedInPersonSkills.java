1f6e0f94c2c4182b9ace666d7cc372876d321b3e
Petko <petko_yanakievv@abv.bg>
2014-05-09 17:09:19 +0300

    Add some comments.

--- LinkedInTest/src/main/java/linkedinsdk/test/LinkedInPersonSkills.java
+++ LinkedInTest/src/main/java/linkedinsdk/test/LinkedInPersonSkills.java
@@ -15,7 +15,7 @@ public class LinkedInPersonSkills {
 	 private static final String API_KEY_SECRET="2Y04Y2fxQMsgDdNf";
 	 private static final String ACC_TOKEN="3f40c383-2cdb-44c1-9dfa-97eee4102b83";
 	 private static final String ACC_TOKEN_SECRET="da6f576a-0021-4641-9165-bd0036c366e0";
-	
+	//just testing file.
 	  public static void main(String[] args)
 	  {
 		final LinkedInApiClientFactory factory = LinkedInApiClientFactory.newInstance(API_KEY,API_KEY_SECRET);
